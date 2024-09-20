18259acb6a241295a5d5a3e418ed6931ebc0b40a
cosmic <cosmic@kali.cosmic>
2024-09-20 09:07:53 -0400

    add more plugins

---
+++ README.md
@@ -0,0 +1,1 @@
+## This is my nvim config in one file (init.lua)
--- init.lua
+++ init.lua
@@ -35,6 +35,13 @@ require("lazy").setup({
     {'akinsho/toggleterm.nvim', version = "*", config = true},
     {'beauwilliams/statusline.lua'},
     {
+  "yorickpeterse/nvim-window",
+  keys = {
+    { "<leader>wj", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
+  },
+  config = true,
+},
+    {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
@@ -62,6 +69,12 @@ require("lazy").setup({
   end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
 },
+{
+  "karb94/neoscroll.nvim",
+  config = function ()
+    require('neoscroll').setup({})
+  end
+},
     {'romgrk/barbar.nvim',
     dependencies = {
       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
@@ -76,6 +89,44 @@ require("lazy").setup({
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
+  {
+    "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    -- Because of the keys part, you will be lazy loading this plugin.
+    -- The plugin wil only load once one of the keys is used.
+    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+    -- or lazy = false. One of both options will work.
+    opts = {
+        -- your configuration comes here
+        -- for example
+        enabled = true,  -- if you want to enable the plugin
+        message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+        date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+        virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+    },
+ 
+},
+{
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
+},
+
     { "nvim-tree/nvim-tree.lua",
       requires = { "nvim-tree/nvim-web-devicons" }, -- Optional, for file icons
       config = function()

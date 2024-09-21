992653497908544817d5399d94eb58988e7b9f87
NM-004 <enembarjo03@gmail.com>
2024-09-21 20:24:50 +0530

    made a separate file for lazy plugin manager

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,7 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true,desc = 'Nvim Tree Open' })
 
 
-
+-- Key mapping for Windows split
 vim.keymap.set('n', '<leader>wh', ':split<CR>', { noremap = true, silent = true, desc = 'Horizontal Split' })
 vim.keymap.set('n', '<leader>wv', ':vsplit<CR>', { noremap = true, silent = true, desc = 'Vertical Split' })
 vim.keymap.set('n', '<leader>wd', ':q<CR>', { noremap = true, silent = true, desc = 'Delete Window' })

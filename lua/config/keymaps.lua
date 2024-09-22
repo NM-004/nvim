f89319e4fd4fb7aac4b4c776907b1337c4238642
Enem <enembarjo03@gmail.com>
2024-09-22 08:22:53 +0530

    improved windows keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,9 +7,10 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent
 
 
 -- Key mapping for Windows split
-vim.keymap.set('n', '<leader>wh', ':split<CR>', { noremap = true, silent = true, desc = 'Horizontal Split' })
-vim.keymap.set('n', '<leader>wv', ':vsplit<CR>', { noremap = true, silent = true, desc = 'Vertical Split' })
-vim.keymap.set('n', '<leader>wd', ':q<CR>', { noremap = true, silent = true, desc = 'Delete Window' })
+vim.keymap.set('n', '<leader>wh', '<C-w>s', { noremap = true, silent = true, desc = 'Horizontal Split' })
+vim.keymap.set('n', '<leader>wv', '<C-w>v', { noremap = true, silent = true, desc = 'Vertical Split' })
+vim.keymap.set('n', '<leader>w=', '<C-w>=', { noremap = true, silent = true, desc = 'Equal Split' })
+vim.keymap.set('n', '<leader>wd', '<cmd>close<CR>', { noremap = true, silent = true, desc = 'Delete Window' })
 
 
 vim.keymap.set('n', '<leader>L', ':Lazy<CR>', {noremap = true, silent = true, desc = 'Lazy Plugin Window'})

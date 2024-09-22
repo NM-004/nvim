ab98504170c25e65f7b919158dd1b23598884b29
Enem <enembarjo03@gmail.com>
2024-09-22 08:37:14 +0530

    more options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,5 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.tabstop = 4       -- Number of spaces a <Tab> character represents
 vim.opt.shiftwidth = 4    -- Number of spaces to use for each indentation level
 vim.opt.expandtab = true  -- Convert tabs to spaces
-
+vim.opt.termguicolors = true
+vim.opt.background = "dark"

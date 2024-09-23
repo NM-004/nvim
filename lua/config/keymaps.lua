e9bbcc5ce4ff981a909165bea3fc784852b6349d
NM-004 <enembarjo03@gmail.com>
2024-09-23 14:18:32 +0530

    updates keymaps and add leetcode keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,27 @@
+local global = vim.g
+local key = vim.keymap
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+global.mapleader = " "
 
 -- Key mapping for Nvim Tree
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true,desc = 'Nvim Tree Open' })
+key.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true,desc = 'Nvim Tree Open' })
 
 
 -- Key mapping for Windows split
-vim.keymap.set('n', '<leader>wh', '<C-w>s', { noremap = true, silent = true, desc = 'Horizontal Split' })
-vim.keymap.set('n', '<leader>wv', '<C-w>v', { noremap = true, silent = true, desc = 'Vertical Split' })
-vim.keymap.set('n', '<leader>w=', '<C-w>=', { noremap = true, silent = true, desc = 'Equal Split' })
-vim.keymap.set('n', '<leader>wd', '<cmd>close<CR>', { noremap = true, silent = true, desc = 'Delete Window' })
+key.set('n', '<leader>wh', '<C-w>s', { noremap = true, silent = true, desc = 'Horizontal Split' })
+key.set('n', '<leader>wv', '<C-w>v', { noremap = true, silent = true, desc = 'Vertical Split' })
+key.set('n', '<leader>w=', '<C-w>=', { noremap = true, silent = true, desc = 'Equal Split' })
+key.set('n', '<leader>wd', '<cmd>close<CR>', { noremap = true, silent = true, desc = 'Delete Window' })
 
 
-vim.keymap.set('n', '<leader>L', ':Lazy<CR>', {noremap = true, silent = true, desc = 'Lazy Plugin Window'})
+key.set('n', '<leader>Lp', ':Lazy<CR>', {noremap = true, silent = true, desc = 'Lazy Plugin Window'})
+
+
+-- Key mapping for leetcode 
+key.set('n', '<leader>lm', ':Leet menu<CR>',{desc = 'Leet menu'})
+key.set('n', '<leader>lc', ':Leet console<CR>',{desc = 'Leet console'})
+key.set('n', '<leader>li', ':Leet info<CR>',{desc = 'Leet info'})
+key.set('n', '<leader>ll', ':Leet lang<CR>',{desc = 'Leet language'})
+key.set('n', '<leader>lr', ':Leet run<CR>',{desc = 'Leet run'})
+key.set('n', '<leader>lt', ':Leet test<CR>',{desc = 'Leet test'})
+key.set('n', '<leader>ls', ':Leet submit<CR>',{desc = 'Leet submit'})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,11 +1,13 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.tabstop = 4       -- Number of spaces a <Tab> character represents
-vim.opt.shiftwidth = 4    -- Number of spaces to use for each indentation level
-vim.opt.expandtab = true  -- Convert tabs to spaces
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.tabstop = 4       -- Number of spaces a <Tab> character represents
+opt.shiftwidth = 4    -- Number of spaces to use for each indentation level
+opt.expandtab = true  -- Convert tabs to spaces
+opt.termguicolors = true
+opt.background = "dark"
+opt.autoindent = true
+opt.smartindent = true

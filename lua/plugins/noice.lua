9bf2ca5d795177d9f9abefb79fb036a2dbb49d6f
NM-004 <enembarjo03@gmail.com>
2024-10-03 19:50:20 +0530

    noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -12,5 +12,21 @@ return{
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-    }
+    },
+    config=function()
+
+        require("noice").setup({
+            messages = {
+            -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+            -- This is a current Neovim limitation.
+            enabled = true, -- enables the Noice messages UI
+            view = "notify", -- disable notifications for normal messages
+            view_error = false, -- disable notifications for errors
+            view_warn = false, -- disable notifications for warnings
+            view_history = "messages", -- still view message history
+            view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+            },
+        -- You can add more configuration options if needed
+        })
+    end
 }

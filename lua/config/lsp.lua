2554ccbedea94cef6e7a2c7a581b160ec036f11a
NM-004 <enembarjo03@gmail.com>
2025-08-17 21:01:22 +0530

    decent lsp autocmp works

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -18,6 +18,25 @@ vim.lsp.config('lua_ls', {
 
 vim.lsp.config('ts_ls',{
 	cmd = {'typescript-language-server', '--stdio'},
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
   filetypes = {'typescript', 'javascript', 'typescriptreact', 'javascriptreact'},
   root_dir = vim.fs.root(0, {'package.json', 'tsconfig.json', '.git'})
 })
@@ -28,19 +47,38 @@ vim.lsp.config('pyright',{
 	root_dir = vim.fs.root(0, {'pyproject.toml', 'setup.py', '.git'})
 })
 
-vim.lsp.enable('luals')
-vim.lsp.enable('tsserver')
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('ts_ls')
 vim.lsp.enable('pyright')
 
+--  vim.api.nvim_create_autocmd('LspAttach', {
+--      group = vim.api.nvim_create_augroup('my.lsp', {}),
+--      callback = function(args)
+	--      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        --if client:supports_method('textDocument/completion') then
+          --vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+        --end
+      --end,
+    --})
 
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('my.lsp', {}),
-      callback = function(args)
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        if client:supports_method('textDocument/completion') then
-          vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
-        end
-      end,
-    })
-
-vim.cmd('set completeopt+=noselect')
+--vim.cmd('set completeopt+=noselect')
+require("luasnip.loaders.from_vscode").lazy_load()
+require("blink.cmp").setup({
+	signature = { enabled = true },
+	completion = {
+		list = {
+			selection = { preselect = true, auto_insert = true }
+		},
+		documentation = { 
+			auto_show = true,
+			auto_show_delay_ms = 500
+		}
+	},
+	menu = {
+		auto_show = true,
+		draw = {
+			treesitter = {'lsp'},
+			columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+		}
+	}
+})

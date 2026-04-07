f3d79f3b59fe56e2552ee8966e234fa8d203fdd3
NM-004 <enembarjo03@gmail.com>
2026-04-07 21:00:26 +0530

    reinstall autotag plugin

--- lua/plugin/nvim-treesitter.lua
+++ lua/plugin/nvim-treesitter.lua
@@ -1,10 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
     require("nvim-treesitter").setup({
       ensure_installed = {
-        "html", "javascript", "typescript", "tsx", "vue", "c", "json5"
+        "html","typescript", "javascript", "typescript", "tsx", "vue", "c", "json5"
       },
       highlight = { enable = true },
       indent = { enable = true }
